0c1891ba28e6e0c0f17ce8eafef07827e9c579d6
Rahul Sahu <rahulsahu9719@gmail.com>
2024-03-02 00:28:18 +0530

    Update apply-operations-to-make-all-array-elements-equal-to-zero.ts

--- practice/ts-practice/leet-code/apply-operations-to-make-all-array-elements-equal-to-zero.ts
+++ practice/ts-practice/leet-code/apply-operations-to-make-all-array-elements-equal-to-zero.ts
@@ -1,3 +1,8 @@
+// https://leetcode.com/problems/apply-operations-to-make-all-array-elements-equal-to-zero/description/
+
+
 function checkArray(nums: number[], k: number): boolean {
 
 };
+
+console.log(checkArray([1,4,5,5,2,1], 3) === true);
