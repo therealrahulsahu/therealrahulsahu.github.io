81fb6f7339e4c712b2c67ec1f2778ac9d4816783
Rahul Sahu <rahulsahu9719@gmail.com>
2024-03-13 00:42:36 +0530

    Create binary-tree-inorder-traversal.ts

---
+++ practice/ts-practice/leet-code/binary-tree-inorder-traversal.ts
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/binary-tree-inorder-traversal
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function(root) {
+    const resp = [];
+    const traverse = node=>{
+        if(node){
+            traverse(node.left);
+            resp.push(node.val);
+            traverse(node.right);
+        }
+    }
+    traverse(root);
+    return resp;
+};
