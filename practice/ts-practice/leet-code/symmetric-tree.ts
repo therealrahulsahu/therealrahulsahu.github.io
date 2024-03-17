f7ad658be514c50b0bbfd7d1f844f53c44d2a7d6
Rahul Sahu <rahulsahu9719@gmail.com>
2024-03-17 14:41:41 +0530

    Create symmetric-tree.ts

---
+++ practice/ts-practice/leet-code/symmetric-tree.ts
@@ -0,0 +1,92 @@
+// https://leetcode.com/problems/symmetric-tree/
+(()=>{
+
+    var isSymmetric1 = function(root) {
+        const preOrder = [];
+    
+        const preT = (node, l)=>{
+            if(node){
+                preT(node.left, true);
+                preOrder.push([node.val, l]);
+                preT(node.right, false);
+            }
+        };
+        preT(root, null);
+    
+        const postOrder = [];
+    
+        const postT = (node, l)=>{
+            if(node){
+                postT(node.right, false);
+                postOrder.push([node.val, l]);
+                postT(node.left, true);
+            }
+        };
+    
+        postT(root, null);
+    
+        const checkSide = (pre, post)=>{
+            for(let i=0;i<pre.length;i++){
+                const [preI, preF] = pre[i];
+                const [postI, postF] = post[i];
+                if(preI!==postI) return false;
+                // @ts-ignore
+                if((preF===null) ^ (postF===null)) return false;
+                if((preF!==null && postF!==null) && !(preF ^ postF)) return false; 
+            }   
+            return true;
+        };
+    
+        return preOrder.length===postOrder.length && checkSide(preOrder, postOrder);
+    };
+
+
+
+    var isSymmetric = function(root) {
+        const preOrder = [];
+    
+        const preT = (node, l)=>{
+            if(node){
+                preT(node.left, true);
+                preOrder.push([node.val, l]);
+                preT(node.right, false);
+            }
+        };
+        preT(root, null);
+    
+        let compare = (pre, post)=>{
+            const [preI, preF] = pre;
+            const [postI, postF] = post;
+            
+            if(preI!==postI) return false;
+            // @ts-ignore
+            if((preF===null) ^ (postF===null)) return false;
+            if((preF!==null && postF!==null) && !(preF ^ postF)) return false;
+    
+            return true;
+        }
+    
+        let flag = true;
+    
+        const postT = (node, l)=>{
+            if(node && flag){
+                postT(node.right, false);
+                
+                if(preOrder.length===0){
+                    flag = false;
+                }else{
+                    if(!compare(preOrder[0], [node.val, l])) flag=false;
+                    preOrder.shift();
+                }
+    
+                postT(node.left, true);
+            }
+        };
+    
+        postT(root, null);
+    
+        return preOrder.length===0 && flag;
+        
+    };
+
+})()
