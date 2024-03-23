6dc9a0ea7908f0c6dc39c85b674c24423b251c80
Rahul Sahu <rahulsahu9719@gmail.com>
2024-03-23 15:00:15 +0530

    Update k-diff-pairs-in-an-array.ts

--- practice/ts-practice/leet-code/k-diff-pairs-in-an-array.ts
+++ practice/ts-practice/leet-code/k-diff-pairs-in-an-array.ts
@@ -5,7 +5,7 @@
  * @param {number} k
  * @return {number}
  */
-var findPairs = function(nums, k) {
+var findPairs1 = function(nums, k) {
     const unq = new Set();
 
     const compare = (a,b)=>{
@@ -23,4 +23,31 @@ var findPairs = function(nums, k) {
     }
 
     return k==0 ? unq.size : unq.size/2;
+};
+
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findPairs = function(nums, k) {
+    if (k < 0) return 0;
+    let uniqueNums = new Set(nums);
+    let count = 0;
+
+    if (k === 0) {
+        let frequencyMap = nums.reduce((acc, num) => {
+            acc[num] = (acc[num] || 0) + 1;
+            return acc;
+        }, {});
+        for (let num in frequencyMap) {
+            if (frequencyMap[num] > 1) count++;
+        }
+    } else {
+        uniqueNums.forEach(num => {
+            if (uniqueNums.has(num + k)) count++;
+        });
+    }
+
+    return count;
 };
