76e10d17f7777d34407202186840517c7d62b9f1
Rahul Sahu <rahulsahu9719@gmail.com>
2025-03-21 00:09:59 +0530

    Resume updated

--- java/Java-Tour/src/main/java/org/tour/Main.java
+++ java/Java-Tour/src/main/java/org/tour/Main.java
@@ -1,15 +1,36 @@
 package org.tour;
 
+import javafx.util.Pair;
+
+import java.util.*;
+
 public class Main {
+
+    int R=0,C=1;
+
+    static int[] getLoc(int key){
+        int row = (n-1)-((key-1)/n);
+        int col;
+        if(((key-1)/n)%2==0){
+            col = (key-1)%n;
+        }else{
+            col = (n-1) - (key-1)%n;
+        }
+        return new int[]{row, col};
+    }
+    static int n;
+
     public static void main(String[] args) {
-        Integer i = 0;
-        System.out.println(i);
+        Map<Integer, List<Integer>> map = new HashMap<>();
 
-        Main.test(i);
-        System.out.println(i);
+        List<Integer> list = new ArrayList<>();
+        list.add(7);
+        map.put(1, list);
+
+        map.computeIfAbsent(1, k->new ArrayList<>()).add(8);
+        map.computeIfAbsent(1, k->new ArrayList<>()).add(9);
+
+        System.out.println(map);
     }
 
-    private static void test(Integer i){
-        i=i+1;
-    }
 }
